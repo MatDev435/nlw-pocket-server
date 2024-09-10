79d8c86791d386fda15e3cc00d78635d57324921
MatDev435 <matheuspsalmazo2@gmail.com>
2024-09-10 12:10:18 -0300

    Database: Create table goalCompletions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,24 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desiredWeeklyFrequency').notNull(),
+  createdAt: timestamp('createdAt', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const gaolCompletions = pgTable('goalCompletions', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goalId')
+    .references(() => goals.id)
+    .notNull(),
   createdAt: timestamp('createdAt', { withTimezone: true })
     .notNull()
     .defaultNow(),
